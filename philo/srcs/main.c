ca2bd7d73178d052f0a2ceec7015168311e8f094
Patricia Monteiro <p.alexandracesar@gmail.com>
2025-03-29 20:42:13 +0000

    a

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -83,29 +83,29 @@ int	philo_thread(t_data *data)
 	return (0);
 }
 
-int	philo_status(t_philo *philo, t_data *data)
-{
-	int	i;
-	i = 0;
-	pthread_mutex_lock(&data->status);
-	if (get_current_time() - data->philo->last_eat > (size_t)data->time_to_die)
-	{
-		pthread_mutex_unlock(&data->status);
-		pthread_mutex_lock(&data->status);
-		data->philo_dead = DEAD;
-		pthread_mutex_unlock(&data->status);
-		return (DEAD);
-	}
-	else if (philo->eat_count == data->must_eat_count && philo->full == 0)
-	{
-		/*pthread_mutex_lock(&data->status);*/
-		/*printf("philo->eat_count: %d\n", philo->eat_count);*/
-		data->philo_full++;
-		philo->full = 1;
-	}
-	if (data->philo_full == data->philo_count)
-		data->philo_dead = FULL;
-	i = data->philo_dead;
-	pthread_mutex_unlock(&data->status);
-	return (i);
-}
+/*int	philo_status(t_philo *philo, t_data *data)*/
+/*{*/
+/*	int	i;*/
+/*	i = 0;*/
+/*	pthread_mutex_lock(&data->status);*/
+/*	if (get_current_time() - data->philo->last_eat > (size_t)data->time_to_die)*/
+/*	{*/
+/*		pthread_mutex_unlock(&data->status);*/
+/*		pthread_mutex_lock(&data->status);*/
+/*		data->philo_dead = DEAD;*/
+/*		pthread_mutex_unlock(&data->status);*/
+/*		return (DEAD);*/
+/*	}*/
+/*	else if (philo->eat_count == data->must_eat_count && philo->full == 0)*/
+/*	{*/
+/*		pthread_mutex_lock(&data->status);*/
+/*		printf("philo->eat_count: %d\n", philo->eat_count);*/
+/*		data->philo_full++;*/
+/*		philo->full = 1;*/
+/*	}*/
+/*	if (data->philo_full == data->philo_count)*/
+/*		data->philo_dead = FULL;*/
+/*	i = data->philo_dead;*/
+/*	pthread_mutex_unlock(&data->status);*/
+/*	return (i);*/
+/*}*/
--- philo/srcs/phil_routine.c
+++ philo/srcs/phil_routine.c
@@ -17,7 +17,6 @@ void	eat(t_philo *philo)
 	if (philo->id % 2 == 0)
 	{
 		pthread_mutex_lock(philo->left_fork);
-		printf("%ld %d %s\n", get_current_time() - philo->data->start_time, philo->id, FORK);
 		pthread_mutex_lock(philo->right_fork);
 	}
 	else
@@ -26,18 +25,21 @@ void	eat(t_philo *philo)
 		pthread_mutex_lock(philo->left_fork);
 	}
 	printf("%ld %d %s\n", get_current_time() - philo->data->start_time, philo->id, FORK);
+	printf("%ld %d %s\n", get_current_time() - philo->data->start_time, philo->id, FORK);
 	/*print_msg(philo, FORK);*/
 	/*print_msg(philo, FORK);*/
 	/*print_msg(philo, EAT);*/
 	printf("%ld %d %s\n", get_current_time() - philo->data->start_time, philo->id, EAT);
 	ft_usleep(philo->data->time_to_eat);
-	pthread_mutex_lock(&philo->data->status);
+	/*pthread_mutex_lock(&philo->data->status);*/
 	philo->last_eat = get_current_time();
+	printf("philo->last_eat: %ld\n", philo->last_eat - philo->data->start_time);
 	if (philo->eat_count < philo->data->must_eat_count)
 		philo->eat_count++;
-	pthread_mutex_unlock(&philo->data->status);
+	/*pthread_mutex_unlock(&philo->data->status);*/
 	pthread_mutex_unlock(philo->left_fork);
 	pthread_mutex_unlock(philo->right_fork);
+	p_sleep(philo);
 }
 
 void	*philo_routine(void *phil_arg)
@@ -45,6 +47,8 @@ void	*philo_routine(void *phil_arg)
 	t_philo	*philo;
 
 	philo = (t_philo *)phil_arg;
+	pthread_mutex_lock(&philo->data->status);
+		pthread_mutex_unlock(&philo->data->status);
 	pthread_mutex_lock(&philo->data->status);
 	philo->last_eat = philo->data->start_time;
 	pthread_mutex_unlock(&philo->data->status);
@@ -54,30 +58,30 @@ void	*philo_routine(void *phil_arg)
 		ft_usleep(philo->data->time_to_die);
 		return (NULL);
 	}
-	if (philo->id % 2 != 0)
-	{
-		pthread_mutex_lock(&philo->data->status);
-		print_msg(philo, THINK);
-		pthread_mutex_unlock(&philo->data->status);
-		ft_usleep(100);
-	}
-	while (1)
-	{
-		pthread_mutex_lock(&philo->data->status);
-		if (philo->data->philo_dead != ALIVE)
-		{
-			pthread_mutex_unlock(&philo->data->status);
-			break ;
-		}
-		pthread_mutex_unlock(&philo->data->status);
-		eat(philo);
-		pthread_mutex_lock(&philo->data->status);
-		p_sleep(philo);
-		/*pthread_mutex_unlock(&philo->data->status);*/
+	/*if (philo->id % 2 != 0)*/
+	/*{*/
+	/*	pthread_mutex_lock(&philo->data->status);*/
+	/*	print_msg(philo, THINK);*/
+	/*	pthread_mutex_unlock(&philo->data->status);*/
+	/*	ft_usleep(100);*/
+	/*}*/
+	/*while (philo->data->philo_dead == ALIVE)*/
+	/*{*/
 		/*pthread_mutex_lock(&philo->data->status);*/
+		/*if (philo->data->philo_dead != ALIVE)*/
+		/*{*/
+		/*	pthread_mutex_unlock(&philo->data->status);*/
+		/*	break ;*/
+		/*}*/
+		pthread_mutex_lock(&philo->data->status);
+		eat(philo);
 		print_msg(philo, THINK);
-		pthread_mutex_unlock(&philo->data->status);
-	}
+		eat(philo);
+pthread_mutex_unlock(&philo->data->status);	
+		/*pthread_mutex_unlock(&philo->data->status);	*/
+	/*}*/
+		/*pthread_mutex_unlock(&philo->data->status);*/
+	
 	return (NULL);
 }
 
@@ -86,20 +90,34 @@ void	philo_monitor(t_data *data)
 	int	i;
 
 	i = 0;
-	while (1)
-	{
-		while (data->philo_dead == ALIVE)
+	/*while (1)*/
+	while (data->philo_dead == ALIVE)
 		{
+			printf("data->philo_dead: %d\n", data->philo_dead);
 			pthread_mutex_lock(&data->status);
 			if (i == data->philo_count)
 				i = 0;
-			philo_status(&data->philo[i], data);
+			printf("data->: %d\n", data->philo_dead);
+			if (get_current_time() - data->philo[i].last_eat > (size_t)data->time_to_die)
+			{
+				data->philo_dead = DEAD;
+				pthread_mutex_unlock(&data->status);
+				break ;
+			}
+			else if (data->philo[i].eat_count == data->must_eat_count && data->philo[i].full == 0)
+			{
+				printf("d>: %d\n", data->philo_dead);
+				data->philo_full++;
+				data->philo[i].full = 1;
+			}
+			printf("aaaaaaaaad>: %d\n", data->philo_dead);
+			if (data->philo_full == data->philo_count)
+				data->philo_dead = FULL;
+			if (data->philo_dead != ALIVE)
+				print_msg(&data->philo[i], DIED);
 			i++;
+			pthread_mutex_unlock(&data->status);
 		}
-
-	}
-	if (data->philo_dead == DEAD)
-		print_msg(&data->philo[i], DIED);
 }
 
 void	p_sleep(t_philo *philo)
@@ -109,4 +127,4 @@ void	p_sleep(t_philo *philo)
 		ft_usleep(philo->data->time_to_die);
 	else
 		ft_usleep(philo->data->time_to_sleep);
-}
+	}
